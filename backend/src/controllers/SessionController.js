06cc98f525923040f753435637948e2f2f180500
Eduardo Castellini <eduardo.castellini@tecnospeed.com.br>
2020-03-28 17:48:24 -0300

    Front-End

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -4,15 +4,15 @@ module.exports = {
   async create(req, res) {
     const {id} = req.body;
 
-    const ong = await connection('ongs')
+    const {name} = await connection('ongs')
     .where('id', id)
     .select('name')
     .first();
 
-    if(!ong) {
+    if(!name) {
       return res.status(400).json({error: 'No ONG found with this ID'});
     }
 
-    return res.json(ong);
+    return res.json({name});
   }
 }
